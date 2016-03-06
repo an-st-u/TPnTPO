98c8c0feac648647180b3dd3e3d6ddef77457be2
an-st-u <an-st.u@yandex.ru>
2016-03-06 23:05:59 +0500

    Update main.cpp

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,1 +1,13 @@
-//
+#include <stdio.h>
+void MaxMin(int a, int b, int& max, int& min);
+void MaxMin(int a, int b, int& max, int& min){
+max=(a<b)?b:a;
+min=(a<b)?a:b;
+}
+void main(){
+4
+int max, min, a=2, b=3;
+MaxMin(a, b, max, min);
+printf("\nmax(%d, %d) = %d", a, b, max);
+printf("\nmin(%d, %d) = %d", a, b, min);
+}
