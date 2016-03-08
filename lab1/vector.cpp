02d88fdb6db290555f6e10bc531bcf0f123197d2
an-st-u <an-st.u@yandex.ru>
2016-03-08 13:31:49 +0500

    Update vector.cpp

--- lab1/vector.cpp
+++ lab1/vector.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 #include <cstdlib>
+#define MAX 2147483647.0
+#define MIN -2147483648.0
 
 using namespace std;
 
 class Vector {
 
-public:
+private:
     int Dim;
     float *x;
+public:
     Vector(){Dim=0; x=NULL;}
     Vector(int Dim);
     Vector(Vector& V);
     Vector(int Dim, float x[]);
+    ~Vector();
+
     float& operator[](int j){
         if(j < 0 || j >= Dim){
             cout << "Out of range"<<endl;
@@ -29,7 +34,7 @@ public:
             x[i] = V.x[i];
         return (*this);
     }
-    ~Vector();
+
     Vector operator+ (const Vector &B){ //A+B
         Vector buf(Dim);
         for(int i=0; i<Dim; i++)
@@ -37,42 +42,62 @@ public:
         return buf;
     }
 
-    Vector operator- (const Vector &B){ //A-B
-        Vector buf(Dim);
+    float operator, (const Vector &B){ //A,B
+        Vector buf(1);
         for(int i=0; i<Dim; i++)
+            buf.x[0]+= x[i]*B.x[i];
+        return buf.x[0];
+    }
+
+    Vector operator- (const Vector &B){ //A-B
+
+        Vector buf(Dim);
+        for(int i=0; i<Dim; i++) {
             buf.x[i] = x[i]-B.x[i];
+        }
         return buf;
     }
+
+    Vector operator- (){ //-A
+
+        Vector buf(Dim);
+        for(int i=0; i<Dim; i++) {
+            buf.x[i] = -x[i];
+        }
+        return buf;
+    }
+
 
     Vector operator* (const Vector &B){ //A*B
         Vector buf(Dim);
-        for(int i=0; i<Dim; i++)
-            buf.x[i] = x[i]*B.x[i];
+        double test;
+        for(int i=0; i<Dim; i++) {
+            test = x[i]*B.x[i];
+            if ((test < MIN) || (test > MAX)) {
+              cout << "Выход за диапазон"<<endl;
+              exit(1);
+            }
+            buf.x[i]=test;
+        }
         return buf;
     }
 
-    /*float& operator, (const Vector &B){ //a+B
+    Vector operator* (int t){ //B*C
         Vector buf(Dim);
-        for(int i=0; i<Dim; i++)
-            buf.x[0] += x[i]*B.x[i];
-        return buf.x[0];
-    }*/
-
-
-    void show() {
-        for(int i=0;i<Dim;i++)
-            cout << x[i] << endl;
+        for(int i=0; i<Dim; i++) {
+            buf.x[i] = x[i]*t;
+        }
+        return buf;
     }
 
+    friend void operator<<(ostream& os, const Vector &B);
     friend Vector operator*(int t, const Vector &B);
-    friend Vector operator-(int t, const Vector &B);
-    friend Vector operator+(int t, const Vector &B);
 
 };
 
-Vector::Vector(int Dim){
-    this->Dim = Dim;
-    x = new float(Dim);
+Vector::Vector(int D){
+    Dim = D;
+    x = new float[Dim];
 };
 
 Vector::~Vector(){
@@ -87,36 +112,29 @@ Vector::Vector(int Dim, float x[]) {
     }
 }
 
-Vector operator* (int t, const Vector &B){
+Vector operator* (int t, const Vector &B){ //C*B
         Vector buf(B.Dim);
         for(int i=0; i<B.Dim; i++)
-            buf.x[i] = (float)t*B.x[i];
+            buf.x[i] = t*B.x[i];
         return buf;
     }
 
-Vector operator+ (int t, const Vector &B){ //a+B
-        Vector buf(B.Dim);
-        for(int i=0; i<B.Dim; i++)
-            buf.x[i] = (float)t+B.x[i];
-        return buf;
-    }
-
-Vector operator- (int t, const Vector &B){ //a+B
-        Vector buf(B.Dim);
-        for(int i=0; i<B.Dim; i++)
-            buf.x[i] = (float)t-B.x[i];
-        return buf;
-    }
-
+void operator<<(ostream& os, const Vector &B) {
+    for (int i=0;i<B.Dim;i++)
+        os << B.x[i] << " ";
+    os << endl;
+}
 
 int  main()
 {
     float x[]={3, 5, 2, 6};
     float y[]={1, 1, 1, -1.4};
-    int d = sizeof(x)/4;
-    Vector A(d, x),B(d,y),C(d);
-    C=A*B;
-    C.show();
+    int d = 4;
+    Vector A(d, x),B(d, y),C(d);
+    C=-A+2*A-A+B*2;
+    cout << C;
+    C[0]=(A,B);
+    cout << C[0] << endl;
     C[6] = 1;
     return 0;
 }
