dab2acbbec69455acb518be0c67965702d109530
yaroslavsapronov <ya_sapronov@mail.ru>
2016-03-21 19:28:11 +0500

    Create Matrix.h

---
+++ lab1/Matrix.h
@@ -0,0 +1,20 @@
+class Matrix{
+	public:
+		int M, N;
+		float **A;
+		Matrix(){ //по умолчанию
+			M=N=0; A=NULL;
+		}
+		Matrix(int _m, int _n); // иниц
+		Matrix(int _m, int _n, float* _A); //иниц
+		Matrix(Matrix& V);//копии
+		Matrix& operator-();
+		Matrix& operator= (Matrix& V);
+		Matrix& operator+(Matrix& V);
+		Matrix& operator-(Matrix& V);
+		Matrix& operator*(float x);
+		~ Matrix ();
+		
+		friend void operator<<(std::ostream& os,const Matrix &V);
+		friend Matrix operator*(int t, Matrix &V);
+};// Matrix
