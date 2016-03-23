ea238b858ea574ff3c921893064d50810720b09d
yaroslavsapronov <ya_sapronov@mail.ru>
2016-03-23 22:11:58 +0500

    Update Main.cpp

--- lab1/result/Main.cpp
+++ lab1/result/Main.cpp
@@ -1,10 +1,15 @@
 #include "Test.h"
 #include <conio.h>
+#include <iostream>
 
 int  main() {
 
+	Test::MyIntTest();
+	Test::MyIntTest2();
+	std::cout << "   "<< std::endl;
 	Test::TestVector();
 	Test::TestMatrix();
+	Test::MyIntTest3();
 	_getch();
 	return 0;
 
