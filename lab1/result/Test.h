09999164378281932f27722f44376bc19352283d
yaroslavsapronov <ya_sapronov@mail.ru>
2016-03-23 22:16:44 +0500

    Update Test.h

--- lab1/result/Test.h
+++ lab1/result/Test.h
@@ -4,7 +4,11 @@ class Test
 public:
 	Test();
 	~Test();
+
+	static void MyIntTest();
+	static void MyIntTest2();
 	static void TestVector();
 	static void TestMatrix();
+	static void MyIntTest3();
 };
 
