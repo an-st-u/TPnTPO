ce2cb287abe12e445f3074fb9776ba764817db41
yaroslavsapronov <ya_sapronov@mail.ru>
2016-04-10 16:33:41 +0500

    Update Matrix.h

--- lab1/result/Matrix.h
+++ lab1/result/Matrix.h
@@ -1,29 +1,29 @@
 #pragma once
 #include <iostream>
-#include "vector.h"
 
-class Matrix {
-private:
-	int M, N;
-	float **A;
-public:
-	Matrix() { 
-		M = N = 0; A = NULL;
-	}
-	Matrix(int, int); 
-	Matrix(int, int, float*); 
-	Matrix(Matrix& V);
-	Matrix operator-();
-	float* operator[](int);
-	Matrix& operator= (Matrix& V);
-	Matrix operator+(Matrix& V);
-	Matrix operator-(Matrix& V);
-	Matrix operator*(float x);
-	Vector operator*(Vector& x);
-	~Matrix();
+class MyInt {
+public: int n, m;
+		MyInt();
+		MyInt(int _n);
+		MyInt(char *str);
+		MyInt(MyInt& B);
+		~MyInt();
+		MyInt& operator= (const MyInt& B);
+		MyInt operator-();
+		MyInt operator-(const MyInt& B);
+		bool operator==(const MyInt B);
+		bool operator>(const MyInt& B);
+		bool operator<(const MyInt B);
+		bool operator>=(const MyInt B);
+		bool operator<=(const MyInt B);
+		bool operator!=(const MyInt B);
+		MyInt operator+(MyInt B);
+		MyInt operator/(MyInt B);
+		MyInt operator*(MyInt B);
+		MyInt operator*(int m);
 
-	friend void operator<<(std::ostream& os, const Matrix &V);
-	friend Matrix operator*(int t, Matrix &V);
+		friend MyInt operator*(int m, const MyInt &B);
+		friend MyInt operator+ (int m, MyInt B); // îáúÿâëåíèå äðóæåñòâåííîé ôóíêöèè
+		friend void operator<<(std::ostream& os, const MyInt &B);
 
-};// Matrix
-
+};// MyInt
