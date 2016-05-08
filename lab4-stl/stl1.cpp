6b1bdfb43b17ff53e3857d969ed9dd68f05205cb
an-st-u <an-st.u@yandex.ru>
2016-05-08 19:29:17 +0500

    Update stl1.cpp

--- lab4-stl/stl1.cpp
+++ lab4-stl/stl1.cpp
@@ -20,9 +20,9 @@ void main() {
     double mass[] = { 1, 3, 5 , 2 , 3.1, 1.1, 3.5, 17.9, 1.2, 3.1 };
     
     vector <double> v1(mass, mass + 10);
-    vector <double> v2(v1.size());
+    vector <double> v2;
     std::sort(v1.begin(), v1.end());
-    copy(v1.begin(), v1.end(), v2.begin());
+    copy(v1.begin(), v1.end(), back_inserter(v2));
     for_each(v2.begin(), v2.end(), vin);
     copy(v1.begin(), v1.end(), ostream_iterator<double>(cout, " "));
     cout << endl;
