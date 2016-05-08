aaf0d16727bb5b1ab3b6ea66dc9698fabbed31e1
an-st-u <an-st.u@yandex.ru>
2016-05-08 19:28:43 +0500

    Update stl2.cpp

--- lab4-stl/stl2.cpp
+++ lab4-stl/stl2.cpp
@@ -10,26 +10,26 @@ using namespace std;
 
 void vin(double &elem) {
 
-   elem = pow(elem, 2);
+    elem =  pow(elem, 2);
 
 }
 
 void main() {
 
-    
+
     double mass[10];
-    
+
     ifstream in;
     in.open("C:\\forN2.txt");
     if (!in.fail()) {
         istream_iterator<double> file_it(in), end_of_stream;
         copy(file_it, end_of_stream, mass);
     }
-    
+
     vector <double> v1(mass, mass + 10);
-    vector <double> v2(v1.size());
+    vector <double> v2;
     std::sort(v1.begin(), v1.end());
-    copy(v1.begin(), v1.end(), v2.begin());
+    copy(v1.begin(), v1.end(), back_inserter(v2));
     for_each(v2.begin(), v2.end(), vin);
     copy(v1.begin(), v1.end(), ostream_iterator<double>(cout, " "));
     cout << endl;
