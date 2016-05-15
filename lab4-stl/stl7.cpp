4cdaf0fbae7220b6fba8429d4ecb7d27129a8d27
JuliBer <yulya.berger.95@mail.ru>
2016-05-15 14:38:18 +0500

    Update stl7.cpp

--- lab4-stl/stl7.cpp
+++ lab4-stl/stl7.cpp
@@ -1,53 +1,70 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//7. Для набора целых чисел из текстового файла построить упорядоченный
+//список всех встречающихся значений, найти их частоты. Результаты вывести
+//на консоль.
+
+
 #include <iostream>
-#include <stdio.h>
+#include <fstream>
+#include <string>
+#include <iterator>
+#include <algorithm>
 #include <list>
+#include <map>
 
 using namespace std;
 
+void readFromFile(string pathToFile);  //считывания с файла
+void showAllCompElement(int element) {cout << element << ' ';}   //вывод всех элементов вектора на консоль
+bool comparisionFunc(const int elem1, const int elem2); //сревнение строк
+void getFreq(int element);  //подсчитывает частоту значений
+void showAllFreqElement();  //выводит отображнения
+
+list<int> listInt;
+map<int,int> valueAndFreq;
+
 int main()
 {
-    FILE *input;
-    char filename[81];
-    list<int> *numbers = new list<int>();
-
-    do
-    {
-        cout << "Enter input-file name:" << endl;
-        cin >> filename;
-        input = fopen(filename, "r");
-    }
-    while(input == NULL);
-
-    while(!feof(input))
-    {
-        int buf;
-        fscanf(input, "%d", &buf);
-        numbers->push_back(buf);
-    }
-
-    fclose(input);
-
-    list<int> *uniqueNumbers = new list<int>();
-    list<int> *frequencies = new list<int>();
-
-    for(list<int>::iterator n = numbers->begin(); n != numbers->end(); n++)
-    {
-        bool exists = false;
-        for(list<int>::iterator un = uniqueNumbers->begin(); un != uniqueNumbers->end() && !exists; un++)
-            if(*n == *un)
-                exists = true;
-        if(!exists)
-            uniqueNumbers->push_back(*n);
-    }
-
-    for(list<int>::iterator un = uniqueNumbers->begin(); un != uniqueNumbers->end(); un++)
-    {
-        frequencies->push_back(0);
-        for(list<int>::iterator n = numbers->begin(); n != numbers->end(); n++)
-            if(*un == *n)
-                frequencies->back()++;
-        cout << *un << " " << frequencies->back() << endl;
-    }
-
+    readFromFile("С\\int.txt");                     //!!путь
+    listInt.sort();
+    cout << "Отсортированный список: ";
+    for_each(listInt.begin(),listInt.end(),showAllCompElement);
+    cout<<endl;
+    for_each(listInt.begin(),listInt.end(),getFreq);
+    showAllFreqElement();
     return 0;
+}
+
+void readFromFile(string pathToFile)
+{
+    ifstream fileIn;
+    fileIn.open(pathToFile);
+    if (fileIn.is_open())
+    {
+        istream_iterator<int> iterBegin(fileIn), iterEnd;
+        copy(iterBegin, iterEnd, back_inserter(listInt));
+        fileIn.close();
+    }
+    else
+    {
+        cout << "Can't open file "+pathToFile;
+    }
+}
+
+bool comparisionFunc (const int elem1, const int elem2)
+{
+    return elem1<elem2;
+}
+
+void getFreq(int element)
+{
+    valueAndFreq[element]=count(listInt.begin(),listInt.end(),element);
+}
+
+void showAllFreqElement()
+{
+    map<int,int>::iterator iterMap;
+    for(iterMap=valueAndFreq.begin();iterMap!=valueAndFreq.end();iterMap++)
+        cout<<"Value: "<<iterMap->first<< '\t'<<"Count: "<<iterMap->second<<endl;
 }
