55f41520fef56a93e43a6b2fce3fbd6d1c242bee
yaroslavsapronov <ya_sapronov@mail.ru>
2016-06-01 00:18:05 +0500

    Update Wheel.cpp

--- LabGraphics/Wheel_car/Wheel.cpp
+++ LabGraphics/Wheel_car/Wheel.cpp
@@ -8,6 +8,7 @@ Wheel::Wheel()
 	this->objects = NULL;
 	this->wheel_angle = 0;
 	this->tire = 5;
+
 }
 
 
@@ -19,6 +20,8 @@ Wheel::Wheel(Graphics ^ g, int x, int y, int r, int tire): Circle(g, x, y, r)
 
 	int angle = 0;
 	int shift = 180 / MAX_RANGS_COUNT_LINES;
+
+	this->objects = new Line[MAX_RANGS_COUNT_LINES];
 
 	for (int i = 0; i < MAX_RANGS_COUNT_LINES; i++) {
 		this->objects[i] = Line(g, (x - (r / 2)*cos(angle*PI/180) + r / 2), y - (r / 2)*sin(angle*PI / 180) + r / 2, x + (r / 2)*cos(angle*PI/180) + r / 2, y + (r / 2)*sin(angle*PI / 180) + r / 2);
@@ -67,19 +70,12 @@ void Wheel::move(int dx, int dy) {
 		wheel_angle = -360;
 	}
 
-	int rr = r / 2;
 	
 	for (int i = 0; i < MAX_RANGS_COUNT_LINES; i++) {
 		
-		this->objects[i].x1 = x - rr * cos(angle*PI/180) + rr;
-		this->objects[i].y1 = y - rr * sin(angle*PI/180) + rr;
-		this->objects[i].x2 = x + rr * cos(angle*PI/180) + rr;
-		this->objects[i].y2 = y + rr * sin(angle*PI/180) + rr;
+		this->objects[i].rotate(angle);
 		angle += shift;
 	}
 
     this->show();
 }
-
-
-
